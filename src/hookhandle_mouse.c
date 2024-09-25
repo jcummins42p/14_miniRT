b6b79219033170fcfbb64901cfaba338fcbabde1
jcummins <jcummins42prague@gmail.com>
2024-09-25 16:48:28 +0200

    Mouse wheel zoom working

--- src/hookhandle_mouse.c
+++ src/hookhandle_mouse.c
@@ -6,7 +6,7 @@
 /*   By: jcummins <jcummins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 13:32:48 by jcummins          #+#    #+#             */
-/*   Updated: 2024/09/25 12:36:32 by jcummins         ###   ########.fr       */
+/*   Updated: 2024/09/25 16:46:04 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,19 @@ int	b_release(int button, void *vars)
 	return (0);
 }
 
+void	m_zoom(t_mlx *mlx, int button)
+{
+	if (button == ON_MOUSEDOWN)
+		mlx->rt->scenes[mlx->rt->curr_scene]->cam.point[_Z] += 1;
+	else if (button == ON_MOUSEUP)
+		mlx->rt->scenes[mlx->rt->curr_scene]->cam.point[_Z] -= 1;
+	render_scene(mlx, mlx->rt->scenes[mlx->rt->curr_scene]);
+}
+
 int	b_press(int button, int x, int y, t_mlx *mlx)
 {
-	(void)button;
+	if (button == ON_MOUSEDOWN || button == ON_MOUSEUP)
+		m_zoom(mlx, button);
 	printf("Mousepress: x:%-4d y:%-4d\n", x, y);
 	if (button == ON_DESTROY)
 		mlx_destroy_window(mlx->mlx, mlx->win);
