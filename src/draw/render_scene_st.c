1a6253f2d5400deae3129a31379e23d368f53f59
jcummins <jcummins42prague@gmail.com>
2024-10-18 10:40:57 +0200

    Renaming draw.c render_scene_st.c to better reflect program structure

---
+++ src/draw/render_scene_st.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akretov <akretov@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/20 16:02:30 by jcummins          #+#    #+#             */
+/*   Updated: 2024/10/17 16:28:08 by jcummins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT.h"
+
+void	render_row(t_mlx *mlx, t_scene *scene, int y)
+{
+	int	x;
+
+	x = 0;
+	while (x < RES_W)
+	{
+		scene->select_type[y][x] = SEL_NONE;
+		scene->screen_object[y][x] = NULL;
+		prep_cam_ray(mlx, scene, x, y);
+		x++;
+	}
+}
+
+int	render_scene_st(t_mlx *mlx, t_scene *scene)
+{
+	int				y;
+
+	y = 0;
+	if (!scene->valid)
+		return (1);
+	if (img_init(mlx, scene->img))
+		return (1);
+	y = -1;
+	while (++y < RES_H)
+		render_row(mlx, scene, y);
+	return (0);
+}
+
+int	render_scene(t_mlx *mlx, t_scene *scene)
+{
+	if (mlx->multithread)
+	{
+		if (render_scene_mt(mlx, scene))
+			return (1);
+	}
+	else
+	{
+		if (render_scene_st(mlx, scene))
+			return (1);
+	}
+	post_process(scene);
+	mlx_put_image_to_window(mlx->mlx, mlx->win, scene->img->img, 0, 0);
+	mlx_destroy_image(mlx->mlx, scene->img->img);
+	return (0);
+}
